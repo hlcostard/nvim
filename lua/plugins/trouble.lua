9df36e5b5f02775e52cb6668247de21faff99386
Heitor Costard <hlcostard@gmail.com>
2024-01-14 09:11:38 -0300

    using lazy for plugins

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        vim.keymap.set("n", "<leader>dd", function()
+            require("trouble").toggle()
+        end)
+        vim.keymap.set("n", "[d", function()
+            require("trouble").next({skip_groups = true, jump = true})
+        end)
+        vim.keymap.set("n", "]d", function()
+            require("trouble").previous({skip_groups = true, jump = true})
+        end)
+    end
+}
